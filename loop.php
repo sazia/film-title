a4953dec0d8e9e3cd89f78fdcd3329e8760a868b
sazia <sazia_11yahoo.com>
2014-07-10 12:02:53 +0600

    even number

---
+++ loop.php
@@ -0,0 +1,16 @@
+<?php
+//$i=0;
+//$j=0;
+echo "even";
+for($i=1;$i<10; $i++ ){
+ //for( $j=1; $j<$i;  $j++)
+ 
+  if(($i%2)==0) 
+  {//$even=0;
+  //$even=$even+$j;
+    echo "<br/>".$i;
+	   }
+   //echo "</t>";
+
+  }
+  ?>
