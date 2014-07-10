07e2ae805c893ac79d1c40835143858b0f3f82f2
sazia <sazia_11yahoo.com>
2014-07-10 13:27:01 +0600

    class declaretion

---
+++ user2..php
@@ -0,0 +1,23 @@
+<?php
+//class declaration
+  Class student{
+ 
+  var_damp($student);
+ //accessing object properties
+  var $name ="";
+  var $email =""; 
+  //accessing class method
+function get name(){
+return $this->name;;
+}  
+  }//user refer in object
+  $user1=new user();
+   $user2=new user();
+  $student=new student();
+  
+var_damp($student); 
+ echo $student->name;
+ echo $student->get name();
+   
+   }
+?>
