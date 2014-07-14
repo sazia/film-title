02e80d7370205e060de18701f9c973121b35a59d
sazia <sazia_11yahoo.com>
2014-07-14 14:13:19 +0600

    session start

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+     
+      session_start();
+     // $a_number = $_SESSION['number1'];
+      //$_SESSION['number1']=$_SESSION['number1'] +10;
+      echo $_SESSION['a_value'];
+      
+        ?>
+    </body>
+</html>
