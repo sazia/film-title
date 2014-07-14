3dc8b5dcbe7eb3fd87cd5f0dede2a2b8da581833
sazia <sazia_11yahoo.com>
2014-07-14 14:09:47 +0600

    create calculator

---
+++ calculator.php
@@ -0,0 +1,33 @@
+<?php
+
+
+class calculator 
+{
+public  $first_no;
+public  $second_no;
+
+   function add_no()        
+    {
+      $add = $this->first_no + $this->second_no;
+       return $add ;             
+    }
+    
+    function subtract_no()        
+    {
+      $subtract = $this->first_no + $this->second_no;
+       return $subtract ;             
+    }
+    
+     function multiply_no()        
+    {
+      $multiply= $this->first_no + $this->second_no;
+       return $multiply ;             
+    }
+    function divide_no()        
+    {
+      $divide = $this->first_no + $this->second_no;
+       return $divide ;             
+    } 
+}
+
+?>
