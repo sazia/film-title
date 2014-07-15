2e9febcd4cd28ca3c932d369f9f9488901c8563c
sazia <sazia_11yahoo.com>
2014-07-15 11:03:54 +0600

    interest calculator

---
+++ Interest_Calculator.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<title></title>
+<head>
+<body>
+    <form action= 'Interest_Calculator.php', method="POST">
+        Principal:<input type="text" , name="Principal"> <br/>
+        Annual Interest Rate::<input type="text" , name=" Interest"> <br/>
+        Time Period (In year):<input type="text" , name=" Year"> <br/>
+        <input type="Submit", value="calculate , name=" calculate "><br/>
+        </form>
+         
+        
+    
+
+<?php
+
+ ?>
+</body>
+</html>
+
